2763836964a90a44c37538e9170a192afaee1ebd
ssethsangoma <99713098+ssethsangoma@users.noreply.github.com>
2022-02-15 18:27:21 +0530

    Add files via upload

---
+++ network_source_routing_policy_retrieve.py
@@ -0,0 +1,39 @@
+#  Retrieve network configuration
+#  sbc --> configuration --> IP setings --> network --> source_policy_routing --> source_policy_routing_any_policy
+#
+
+import pycurl, json
+from io import BytesIO
+#from urllib import urlencode	 REMOVED since python3 does not support
+
+try:
+  option = json.load(open("config.json"))
+
+  response=BytesIO()
+  base_url = 'http://{0}/SAFe/sng_rest/api/'.format(option["SERVER"])
+  method_name = "retrieve"
+  module_name = "network"
+  obj_type = "source_policy_routing"
+  obj_name = "source_policy_routing_88"
+  url = base_url+method_name+'/'+module_name+'/'+obj_type+'/'+obj_name
+
+  headers = {'Content-Type': 'application/json',}
+
+  c = pycurl.Curl()
+  c.setopt(pycurl.WRITEFUNCTION, response.write)
+
+  c.setopt(pycurl.URL, url)
+  if(option["API_KEY"]):
+    c.setopt(pycurl.HTTPHEADER, ['X-API-KEY: {0}'.format(option["API_KEY"]),'Accept: application/json'])
+  else:
+    c.setopt(pycurl.HTTPHEADER, ['Accept: application/json'])
+  c.perform()
+  data=response.getvalue()
+  print(data)
+except:
+  exdata=response.getvalue()
+  print("exeption has been found")
+  print(exdata)
+finally:
+  c.close()
+
